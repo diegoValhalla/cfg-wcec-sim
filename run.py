38177ce4bdef86d657ee79719d455da2ff74f1c3
Diego Pinheiro <diegoquintanap@gmail.com>
2015-04-13 08:43:48 -0400

    Gather simulation frequency and print it
    The most important information of each simulation
    is computed and print to user. The current,
    valentin and koreans proposals are all done only
    for worst and best path. Average path is missing.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, '../src')
 
 from cfg import cfg, cfg2graphml
-from sim import cfg_paths
+from sim import cfg_paths, sim
 
 
 def run(filename):
@@ -15,37 +15,62 @@ def run(filename):
     graphml = cfg2graphml.CFG2Graphml()
     graphml.make_graphml(graph, file_name='opa.graphml', yed_output=True)
 
-    # CFG paths
+    simulate()
+
+def simulate():
     cfgpaths = cfg_paths.CFGPaths()
+    simulate = sim.SimDVFS(deadline, freqs_volt)
 
-    # get worst path
+    # get worst path for current proposal, valentin and koreans
     wpath = cfgpaths.find_worst_path(graph)
-    sys.stdout.write('RWCEC: ' + str(wpath.get_path_rwcec()) + '\n')
-    sys.stdout.write('Path: ')
-    for node, wcec in wpath.get_path():
-        sys.stdout.write(str(node.get_start_line()) + ', ')
-    sys.stdout.write('\n')
+    write_path(wpath)
+    worst_result = simulate.start_sim(wpath, init_freq,
+            valentin=False, koreans=False)
+    worst_vresult = simulate.start_sim(wpath, init_freq,
+            valentin=True, koreans=False)
+    worst_kresult = simulate.start_sim(wpath, 0,
+            valentin=False, koreans=True)
 
-    # get best path
+    print_result(wpath.get_path_rwcec(), worst_result, worst_vresult,
+            worst_kresult)
+
+    # get best path for current proposal, valentin and koreans
     bpath = cfgpaths.find_best_path(graph)
-    sys.stdout.write('RWCEC: ' + str(bpath.get_path_rwcec()) + '\n')
+    write_path(bpath)
+    best_result = simulate.start_sim(bpath, init_freq,
+            valentin=False, koreans=False)
+    best_vresult = simulate.start_sim(bpath, init_freq,
+            valentin=True, koreans=False)
+    best_kresult = simulate.start_sim(bpath, 0,
+            valentin=False, koreans=True)
+
+    print_result(bpath.get_path_rwcec(), best_result, best_vresult,
+            best_kresult)
+
+    # get middle path for current proposal, valentin and koreans
+    mpath = cfgpaths.find_mid_path(graph, wpath.get_path_rwcec(),
+            bpath.get_path_rwcec())
+    while mpath is not None:
+        write_path(mpath)
+        mpath = cfgpaths.find_mid_path(graph, mpath.get_path_rwcec(),
+                bpath.get_path_rwcec())
+
+def write_path(path):
+    sys.stdout.write('RWCEC: %d\n' % path.get_path_rwcec())
     sys.stdout.write('Path: ')
-    for node, wcec in bpath.get_path():
+    for node, wcec in path.get_path():
         sys.stdout.write(str(node.get_start_line()) + ', ')
     sys.stdout.write('\n')
 
-    # get middle path
-    while True:
-        mpath = cfgpaths.find_mid_path(graph, wpath.get_path_rwcec(),
-                bpath.get_path_rwcec())
-        if mpath is None:
-            break
-        sys.stdout.write('RWCEC: ' + str(mpath.get_path_rwcec()) + '\n')
-        sys.stdout.write('Path: ')
-        for node, wcec in mpath.get_path():
-            sys.stdout.write(str(node.get_start_line()) + ', ')
-        sys.stdout.write('\n')
-        wpath = mpath
+def print_result(path_rwcec, result, vresult, kresult):
+    # print results for current valentin and koreans proposals
+    simulate.print_results(path_rwcec, result)
+    simulate.print_results(path_rwcec, vresult)
+    simulate.print_results(path_rwcec, kresult)
+
+    simulate.compare_result_to_worst_freq(path_rwcec, result)
+    simulate.compare_result_to_worst_freq(path_rwcec, vresult)
+    simulate.compare_result_to_worst_freq(path_rwcec, kresult)
 
 
 if __name__ == '__main__':
@@ -54,4 +79,3 @@ if __name__ == '__main__':
         print('Too few arguments')
     else:
         run(sys.argv[1])
-
--- sim/sim.py
+++ sim/sim.py
@@ -7,38 +7,48 @@ from cfg.cfg_nodes import CFGNodeType, CFGEntryNode, CFGNode
 
 
 class SimDVFS(object):
-    def __init__(self, deadline=0, freqs_available=[]):
+    def __init__(self, deadline=0, freqs_volt={}):
+        self._freqs_available = list(freqs_volt.keys())
+        self._freqs_volt = freqs_volt
         self._deadline = deadline
-        self._curfreq = 0
-        self._freqs_available = freqs_available
-        self._cycles_consumed = 0
-        self._freq_cycles_consumed = []
         self._typeB_overhead = 0
         self._typeL_overhead = 0
 
-    def start_sim(self, path, init_freq=0):
+    def _init_data(self):
+        self._curfreq = 0
+        self._cycles_consumed = 0
+        self._freq_cycles_consumed = []
+
+    def start_sim(self, path, init_freq=0, valentin=False, koreans=False):
         """ Explore all functions in the C code
 
             Args:
                 graph (cfg.CFG): CFG of the given C file
         """
-        self._curfreq = init_freq
+        self._init_data()
+        if koreans:
+            self._curfreq = path.get_path_rwcec() / self._deadline
+        else:
+            self._curfreq = init_freq
         for i in range(0, len(path)):
             n, wcec = path[i]
             self._cycles_consumed += wcec
-            if i + 1 < len(path): # check if n is not last node
+            # check if n is not last node
+            if valentin == False and i + 1 < len(path):
                 child = path[i + 1]
                 if (n.get_type() == CFGNodeType.IF
                         or n.get_type() == CFGNodeType.ELSE_IF):
-                    self._check_typeB_edge(n, child)
+                    self._check_typeB_edge(n, child, koreans)
                 elif n.get_type() == CFGNodeType.PSEUDO:
-                    self._check_typeL_edge(n, child)
+                    self._check_typeL_edge(n, wcec, child, koreans)
 
         # store last information
         if self._cycles_consumed != 0:
             self._update_data(self._curfreq)
 
-    def _check_typeB_edge(self, n, child):
+        return self._freq_cycles_consumed
+
+    def _check_typeB_edge(self, n, child, koreans=False):
         """ Check if current child has a RWCEC less than the greatest RWCEC of
             a successor of current node. If it is, so this is a type-B edge.
 
@@ -56,14 +66,14 @@ class SimDVFS(object):
         bjline = child.get_start_line()
         if rwcec_bj < rwcec_succbi:
             ratio = self._compute_typeB_sur(rwcec_succbi, rwcec_bj)
-            self._change_freq(ratio)
+            self._change_freq(ratio, koreans)
 
     def _compute_typeB_sur(self, rwcec_wsbi, rwcec_bj):
         if rwcec_wsbi - self._typeB_overhead <= 0:
             return 1
         return float(rwcec_bj) / (rwcec_wsbi - self._typeB_overhead)
 
-    def _check_typeL_edge(self, n, loop_wcec, child):
+    def _check_typeL_edge(self, n, loop_wcec, child, koreans=False):
         """ Get loop information from current node and child and add DVFS code
 
             Args:
@@ -82,11 +92,12 @@ class SimDVFS(object):
 
         ratio = self._compute_typeB_sur(loop_wcec_once, loop_after_rwcec,
                 loop_max_iter, runtime_iter)
-        self._change_freq(ratio)
+        self._change_freq(ratio, koreans)
 
     def _compute_typeL_sur(self, loop_wcec_once, loop_after_rwcec,
                 loop_max_iter, runtime_iter):
-        saved = self._compute_typeL_cycles_saved()
+        saved = self._compute_typeL_cycles_saved(loop_wcec_once,
+                loop_max_iter, runtime_iter)
         if loop_after_rwcec + saved - self._typeL_overhead <= 0:
             return 1
         return (float(loop_after_rwcec) /
@@ -96,17 +107,22 @@ class SimDVFS(object):
             runtime_iter):
         return loop_wcec_once * (loop_max_iter - runtime_iter)
 
-    def _change_freq(self, ratio):
+    def _change_freq(self, ratio, koreans=False):
         if ratio >= 1: return
         newfreq = self._curfreq * ratio
         newfreq = math.ceil(newfreq)
         if newfreq < self._curfreq:
-            set_freq = 0
-            for freq in self._freqs_available: # check for an available freq
-                if freq >= newfreq and (set_freq == 0 or freq <= set_freq):
-                    set_freq = freq
-            if set_freq != 0: # if freq is available
+            if koreans:
                 self._update_data(set_freq)
+            else:
+                # check for an available freq
+                set_freq = 0
+                for freq in self._freqs_available:
+                    if freq >= newfreq and (set_freq == 0 or freq <= set_freq):
+                        set_freq = freq
+                # if freq is available
+                if set_freq != 0:
+                    self._update_data(set_freq)
 
     def _update_data(self, newfreq):
         data = (self._curfreq, self._cycles_consumed)
@@ -114,6 +130,37 @@ class SimDVFS(object):
         self._curfreq = newfreq
         self._cycles_consumed = 0
 
-    def print_results(self):
+    def print_results(self, path_rwcec, freq_cycles_consumed):
         result = ''
+        total_time = 0
+        total_energy = 0
+        for freq, cycles in freq_cycles_consumed:
+            time = float(cycles) / freq
+            energy = cycles * self.freqs_volt[freq]
+            result += 'F: %(freq)d%(n)s'
+            result += '  Cycles: %(cycles)d%(n)s'
+            result += '  Time: %(time)d%(n)s'
+            result += '  Energy: %(energy)d%(n)s%(n)s'
+            total_time += time
+            total_energy += energy
 
+        result += 'Deadline: %(deadline)d%(n)s'
+        result += '  Total Time: %(total_time)d%(n)s'
+        result += 'RWCEC: %(path_rwcec)d%(n)s'
+        result += 'Total Energy: %(total_energy)d%(n)s'
+
+        result = result % ({
+            'freq': freq, 'cycles': cycles, 'time': time, 'energy': energy,
+            'deadline': self._deadline, 'total_time': total_time,
+            'path_rwcec': path_rwcec, 'total_energy': total_energy,
+            'n', '\n'
+        })
+        print result
+
+    def compare_result_to_worst_freq(self, path_rwcec, freq_cycles_consumed):
+        worst_freq = max(self._freqs_available)
+        worst_energy = path_rwcec * self.freqs_volt[worst_freq]
+        for freq, cycles in freq_cycles_consumed:
+            energy = cycles * self.freqs_volt[freq]
+        print ('Energy reduction based on worst frequency: %f%%' %
+                (energy * 100) / worst_energy))
